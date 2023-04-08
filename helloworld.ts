6805e0e750ce8b5a57b5e5ae8f9dff12609b363d
M_Murata <g107141m@gmail.com>
2023-04-08 15:59:01 +0900

    first commit

--- helloworld.ts
+++ helloworld.ts
@@ -2,3 +2,6 @@ const message1: string = 'Hello World 1';
 console.log(message1);
 const message2: string = 'Hello World 2';
 console.log(message2);
+
+const message_murata: string = 'Hello World Murata';
+console.log(message_murata);
